7744fb8bae9e1653da3f053c25c3ed2a6c0c8ccf
miklu612 <>
2023-05-17 12:28:57 +0300

    Implemented support for a global margin

--- lua/margin/init.lua
+++ lua/margin/init.lua
@@ -1,5 +1,10 @@
 local Margin = {
-    text_width = 80
+    text_width = 80,
+
+    -- Should we check every line, or only the current line
+    -- true : check every
+    -- false : check only the current
+    global_margin = false
 }
 
 -- Enables the margin
@@ -21,6 +26,22 @@ function Margin.EnableIfRequired(line)
     end
 end
 
+function Margin.CheckGlobalMargin()
+
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+
+    Margin.Disable()
+
+    for index, line in pairs(lines) do
+        if #line > Margin.text_width then
+            Margin.Enable()
+            break
+        end
+    end
+
+
+end
+
 -- Gets the current line, and runs `Margin.EnableIfRequired` which checks should
 -- the margin be enabled
 function Margin.CheckCurrentLine() 
@@ -37,11 +58,19 @@ function Margin.setup(options)
         Margin.text_width = options.text_width
     end
 
+    if options.global_margin ~= nil then
+        Margin.global_margin = options.global_margin
+    end
+
     vim.api.nvim_create_autocmd(
         {"TextChangedI", "TextChanged"},
         {
             callback = function() 
-                Margin.CheckCurrentLine()
+                if Margin.global_margin == false then
+                    Margin.CheckCurrentLine()
+                else
+                    Margin.CheckGlobalMargin()
+                end
             end
         }
     )
